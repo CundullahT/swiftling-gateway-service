16775c4ea412cc397816a5035f0cb9bfdffb2a37
CundullahT-CT <cterzioglu@cybertekschool.com>
2025-06-30 15:20:15 +0200

    adds cors config

--- src/main/java/com/swiftling/config/SecurityConfig.java
+++ src/main/java/com/swiftling/config/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -37,8 +43,29 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.GET, freeResourceURLs).permitAll()
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .cors(corsSpec -> corsSpec.configurationSource(corsConfigurationSource()))
                 .build();
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:8762",
+                "http://cundi.onthewifi.com:8762",
+                "http://localhost:5000",
+                "http://cundi.onthewifi.com:5000",
+                "https://swiftlingapp.com",
+                "https://www.swiftlingapp.com"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
